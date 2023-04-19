a76abedc292d4ad3d3e9a93182e1f5944c4196ae
Dwebers <129147769+Dwebers@users.noreply.github.com>
2023-04-19 08:22:20 +0200

    Update Feld.java

--- Schiffe Versenken/Feld.java
+++ Schiffe Versenken/Feld.java
@@ -12,19 +12,37 @@ public class Feld
         gegnerFeld = new int[10][10];
         eigenesFeld = new int[10][10];
     }
-    public void trefferEigenesFeld(int x, int y){
+    public int trefferEigenesFeld(int x, int y){
     
         if(eigenesFeld[x][y]== 2)
         {
             eigenesFeld[x][y] = 3;
+            return 3;
             
         }
          else if(eigenesFeld[x][y] ==0)
          {
             eigenesFeld[x][y]=1;
             Spiel.beendeZug();
+             return 1;
          }  
-        
-     
+        return -1; // 3= Treffer, 1 ist kein Treffer, -1 ist ein invalider Schuss
+    
+    }
+    public int gibFeldPosition(int x, int y){
+    
+        return eigenesFeld[x][y];
+    }
+    public int gibGegnerFeldPosition(int x, int y){
+    
+        return gegnerFeld[x][y];
+    }
+    public void setFeldPosition(int x, int y, int neuesInt){
+    
+         eigenesFeld[x][y]= neuesInt ;
+    }
+    public void setGegnerFeldPosition(int x, int y, int neuesInt){
+    
+        gegnerFeld[x][y] = neuesInt;
     }
 }
