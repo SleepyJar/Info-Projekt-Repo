7c6bb0415e6c9b329cc341bbc9499c0964ec73fc
Dwebers <129147769+Dwebers@users.noreply.github.com>
2023-04-19 07:50:47 +0200

    Add files via upload

---
+++ Schiffe Versenken/Feld.java
@@ -0,0 +1,21 @@
+
+
+public class Feld
+{
+    private int zugehörigerSpieler;
+    private int [][]gegnerFeld ;
+    //0 = leeres Feld, 1 = getroffenes leeres Feld, 2 getroffenes Schiff, 3 gesunkenes SChiff
+     private int [][] eigenesFeld;
+    //0 leeres Feld, 1 getroffenes leeres Feld, 2 eigenes Schiff, 3 getroffenes Schiff
+    public Feld(int Spieler){
+    
+        gegnerFeld = new int[10][10];
+        eigenesFeld = new int[10][10];
+    }
+    public void trefferEigenesFeld(int x, int t){
+    
+    if(
+    
+    )
+    }
+}
