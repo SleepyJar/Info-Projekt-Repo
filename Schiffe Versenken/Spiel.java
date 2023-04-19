d28c404b98393a314dc180ae8fe27b77b5547372
Download <94560499+SleepyJar@users.noreply.github.com>
2023-04-19 08:17:16 +0200

    Create Spiel.java

---
+++ Schiffe Versenken/Spiel.java
@@ -0,0 +1,14 @@
+public class Spiel{
+    private Spieler spieler1;
+    private Spieler spieler2;
+    private int aktuellerSpieler;
+    private View view;
+    
+    public Spiel(){
+        spieler1 = new Spieler();
+        spieler2 = new Spieler();
+        aktuellerSpieler = 1;
+    }
+    
+    
+}
