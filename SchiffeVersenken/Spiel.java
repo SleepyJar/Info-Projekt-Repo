bc1473661cb45eb86fa6954a7e3d002d1670cd41
Dwebers <129147769+Dwebers@users.noreply.github.com>
2023-05-02 08:27:57 +0200

    Update Spiel.java

--- SchiffeVersenken/Spiel.java
+++ SchiffeVersenken/Spiel.java
@@ -1,26 +1,29 @@
 public class Spiel{
-    private Spieler spieler1;
-    private Spieler spieler2;
+    private Spieler[] spieler;
+   
     private static int aktuellerSpieler;
     private int aktuellesFeld; //aktuelles Feld der beiden Felder
     private View view;
     
     public Spiel(){
-        spieler1 = new Spieler();
-        spieler2 = new Spieler();
+        spieler = new Spieler[2]
+        spieler[0] = new Spieler();
+        spieler[1] = new Spieler();
         aktuellerSpieler = 1;
         aktuellesFeld = 1;
     }
     
     public static void beendeZug(){
     
-        if(aktuellerSpieler == 1){
-            aktuellerSpieler = 2;
+        if(aktuellerSpieler == 0){
+            aktuellerSpieler = 1;
         }
         else
         {
-            aktuellerSpieler =1;
+            aktuellerSpieler =0;
         }
         
     }
+    
+    public void feldUpdateModel{}
 }
