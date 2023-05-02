23bd68676bc2beac03f1469572309e3debc2c5ed
Download <94560499+SleepyJar@users.noreply.github.com>
2023-05-02 08:29:37 +0200

    Update View.java

--- SchiffeVersenken/View.java
+++ SchiffeVersenken/View.java
@@ -2,25 +2,32 @@ import javax.swing.*;
 import java.awt.Dimension;
 
 public class View extends JFrame{
-     private javax.swing.JPanel jPanel1 = new javax.swing.JPanel();
+     private JPanel jPanel1 = new JPanel();
  
     
-    private javax.swing.JButton[] jButton = new javax.swing.JButton[169];
- 
+    private JButton[] jButton = new JButton[100];
+    private JLabel[] jLabeloben = new JLabel[10]; 
+    private JLabel[] jLabelunten = new JLabel[10];
+    private JLabel[] jLabelrechts = new JLabel[10];
+    private JLabel[] jLabelinks = new JLabel[10];
     
     public View() {
         setPreferredSize(new Dimension(1920, 1080));
         this.setVisible(true);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        jPanel1.setLayout( new java.awt.GridLayout( 13, 13 ) );
+        jPanel1.setLayout( new java.awt.GridLayout( 10, 10 ) );
+        jPanel1.setBorder(BorderFactory.createEmptyBorder(70, 70, 70, 70));
         ButtonListener bl = new ButtonListener();
         
-        for ( int i = 0; i<169; i++ ) {
-            jButton[i] = new javax.swing.JButton ( "JButton" + (i+1) );
+        for ( int i = 0; i<100; i++ ) {
+            jButton[i] = new JButton ();
             jButton[i].addActionListener ( bl );
             jPanel1.add ( jButton[i] );
         }
- 
+        
+        for(int i = 0; i > 10; i++){
+            
+        }
         
         this.getContentPane().add ( jPanel1 ) ;
  
