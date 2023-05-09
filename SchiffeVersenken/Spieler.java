721abace4f8c0ce9deef95139e4f040325496b39
Dwebers <129147769+Dwebers@users.noreply.github.com>
2023-05-09 08:30:02 +0200

    Update Spieler.java

--- SchiffeVersenken/Spieler.java
+++ SchiffeVersenken/Spieler.java
@@ -3,66 +3,64 @@ public class Spieler{
        private int spielernummer;
         public Feld spielerFeld;
         public int anzahlaufzustellendeSchiffe; //damit bei schiffeAufstellen das richtige gesetzt wird, -1 ist nach vollendung desSchiffebauprozesses
-        public Schiff uboot1;
-        public Schiff uboot2;
-        public Schiff uboot3;
-        public Schiff uboot4;
-        public Schiff zerstoerer1;
-        public Schiff zerstoerer2;
-        public Schiff zerstoerer3;
-        public Schiff kreuzer1;
-        public Schiff kreuzer2;
-        public Schiff schlachtschiff;
+      
+        public Schiff[] eigeneSchiffe;
       public Spieler(int spielernr){
           spielernummer = spielernr;
           spielerFeld = new Feld();
           anzahlaufgestellteSchiffe = 9;
+          eigeneSchiffe = new Schiff[10];
         
       }
       public Feld gibFeld(){
         
         return spielerFeld;
         }
-        public void schiffeAufstellen(int x, int y,String Ausrichtung){
+      public Schiff gibSchiff(int no){ //von 0-9
+      
+        return eigeneSchiffe[no];
+      
+      }
+        public boolean schiffeAufstellen(int x, int y,String Ausrichtung){ //aktivieren bei Klick. Boolean ist wahr, wenn die Position invalide ist
         switch(anzahlaufgestellteSchiffe){ 
             case 0: 
-            uboot1 = new Schiff(2, x, y, Ausrichtung);
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(2, x, y, Ausrichtung);
             anzahlaufzustellendeSchiffe--;
             break;
             case 1: 
-            uboot2 = new Schiff(2, x, y, Ausrichtung);
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(2, x, y, Ausrichtung);
                 anzahlaufzustellendeSchiffe--;
             break;
             case 2: 
-            uboot3 = new Schiff(2, x, y, Ausrichtung);
-                aanzahlaufzustellendeSchiffe--;
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(2, x, y, Ausrichtung);
+                anzahlaufzustellendeSchiffe--;
             break;
             case 3: 
-            uboot4 = new Schiff(2, x, y, Ausrichtung);
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(2, x, y, Ausrichtung);
                 anzahlaufzustellendeSchiffe--;
             break;
             case 4:
-            zerstoerer1 = new Schiff(3, x, y, Ausrichtung);
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(3, x, y, Ausrichtung);
                 anzahlaufgestellteSchiffe--;
             break;
             case 5:
-            zerstoerer2 = new Schiff(3, x, y, Ausrichtung);
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(3, x, y, Ausrichtung);
                 anzahlaufgestellteSchiffe--;
             break;
             case 6:
-            zerstoerer3 = new Schiff(3, x, y, Ausrichtung);
+            eigeneSchiffe[anzahlaufgestellteSchiffe]= new Schiff(3, x, y, Ausrichtung);
                anzahlaufzustellendeSchiffe--;
             break;
             case 7:
-            kreuzer1 = new Schiff(4, x, y, Ausrichtung);
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(4, x, y, Ausrichtung);
                  anzahlaufzustellendeSchiffe--1
             break;
             case 8:
-            kreuzer2 = new Schiff(4, x, y, Ausrichtung);
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(4, x, y, Ausrichtung);
                 anzahlaufzustellendeSchiffe--;
             break;
             case 9:
-            schlachtschiff = new Schiff(5, x, y, Ausrichtung);  
+            eigeneSchiffe[anzahlaufgestellteSchiffe] = new Schiff(5, x, y, Ausrichtung);  
                 
             Spiel.beendeZug();
             anzahlaufzustellendeSchiffe== -1;
@@ -71,7 +69,8 @@ public class Spieler{
             
             default: 
             System.out.println("Fehler"); 
-            
+            }
+            return baueSchiff(anzahlaufgestellteSchiffe++);
         } 
         
     public String naechstesSchiff geben(){
@@ -88,7 +87,7 @@ public class Spieler{
             return "U-Boot, 2";
             break;
             case 3: 
-            eturn "U-Boot, 2";;
+            return "U-Boot, 2";;
             break;
             case 4:
             return "Zerstoerer, 3";
@@ -111,5 +110,20 @@ public class Spieler{
             
             default: 
             System.out.println("Fehler"); 
+    }}
+    
+    public boolean baueSchiff(int noSchiff){ 
+    // setzt ein bestimmtes Schiff im eigenem Feld um
+        if(eigeneSchiffe[noSchiff] == "vertikal"){
+        
+            if(eigeneSchiffe[noSchiffe].10-eigeneSchiffe[noSchiff].laengeGeben 
+        }
+        else{
+        
+        
+        }    
+      
+    
     }
+    
 }
