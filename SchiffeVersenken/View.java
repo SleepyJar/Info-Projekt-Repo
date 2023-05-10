4a19ab553e397028eee8116aca4b1615891e21f6
Download <94560499+SleepyJar@users.noreply.github.com>
2023-05-10 08:34:43 +0200

    Update View.java

--- SchiffeVersenken/View.java
+++ SchiffeVersenken/View.java
@@ -1,12 +1,11 @@
+import java.awt.*;
 import javax.swing.*;
 import java.awt.Dimension;
 
 public class View extends JFrame{
-    private JPanel jPaneloben = new JPanel();
-    private JPanel jPanelunten = new JPanel();
-    private JPanel jPanelrechts = new JPanel();
- 
-    
+    private JPanel jPaneloben;
+    private JPanel jPanelunten;
+    private JPanel jPanelrechts;
     private JButton[] jButtonoben = new JButton[100];
     private JButton[] jButtonunten = new JButton[100];
     private JLabel[] jLabeloben = new JLabel[10]; 
@@ -14,59 +13,49 @@ public class View extends JFrame{
     private JLabel[] jLabelrechts = new JLabel[10];
     private JLabel[] jLabelinks = new JLabel[10];
     private JLabel jLabel;
-    
+ 
     public View() {
-        setPreferredSize(new Dimension(1920, 1080));
+        setSize(new Dimension(1920, 1080));
         this.setVisible(true);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
-        jPaneloben.setLayout( new java.awt.GridLayout( 10, 10 ) );
-        jPaneloben.setLocation(0,0);
-        jPanelunten.setLayout( new java.awt.GridLayout( 10, 10 ) );
-        jPaneloben.setLocation(0,500);
-        ButtonListener bl = new ButtonListener();
-        jLabel = new JLabel("test");
-        jLabel.setBounds(50, 50, 50, 50);
-        add(jLabel);
+        
+        jPaneloben = new JPanel();
+        jPanelunten = new JPanel();
+        jPanelrechts = new JPanel();
+        
+        jPaneloben.setBackground(Color.green);
+        jPaneloben.setLayout(new GridLayout(10, 10));
+        jPaneloben.setSize(1600, 540);
+        jPaneloben.setVisible(true);
+        
+        jPanelunten.setBackground(Color.red);
+        jPanelunten.setLayout(new GridLayout(10, 10));
+        jPanelunten.setLocation(0, 540);
+        jPanelunten.setSize(1600, 540);
+        jPanelunten.setVisible(true);
+        
+        jPanelrechts.setBackground(Color.yellow);
+        jPanelrechts.setLayout(new GridLayout(0, 3));
+        jPanelrechts.setLocation(1600, 0);
+        jPanelrechts.setSize(320, 1080);
+        jPanelrechts.setVisible(true);
+        
+        
         
         for ( int i = 0; i<100; i++ ) {
             jButtonoben[i] = new JButton ();
-            jButtonoben[i].addActionListener ( bl );
             jPaneloben.add ( jButtonoben[i] );
-        }
+        }        
         
-        for ( int i = 0; i<100; i++ ) {
+         for ( int i = 0; i<100; i++ ) {
             jButtonunten[i] = new JButton ();
-            jButtonunten[i].addActionListener ( bl );
             jPanelunten.add ( jButtonunten[i] );
         }
-        
-        for(int i = 0; i > 10; i++){
-            jLabeloben[i] = new JLabel(""+(i + 1));
-            jLabeloben[i].setBounds(30* (i + 1), 30, 30, 30);
-            jLabeloben[i].setVisible(true);
-            add(jLabeloben[i]);
-        }
-        
-        this.getContentPane().add ( jPaneloben ) ;
  
-        // Alle Elemente auf kleinstmögliche Größe bringen
-        pack();
- 
-        
-    }
- 
-    
-    class ButtonListener implements java.awt.event.ActionListener {
-        public void actionPerformed(java.awt.event.ActionEvent e) {
- 
-
-            for (int i=0; i<169; i++) {
-                if( e.getSource() == jButtonoben[i] ){
-                    System.out.println("JButton" + (i+1) + " wurde geklickt.");
-                }
-            }
-        }
+        add(jPaneloben);
+        add(jPanelunten);
+        add(jPanelrechts);
     }
  
     
