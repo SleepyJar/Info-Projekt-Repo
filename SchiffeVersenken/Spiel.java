775062f0d8a61744baf49c68e2d0a60a9c402695
Download <94560499+SleepyJar@users.noreply.github.com>
2023-05-23 08:26:07 +0200

    Update Spiel.java

--- SchiffeVersenken/Spiel.java
+++ SchiffeVersenken/Spiel.java
@@ -1,10 +1,10 @@
 public class Spiel{
     private Spieler[] spieler;
-   
     private static int aktuellerSpieler;
     private int aktuellesFeld; //aktuelles Feld der beiden Felder
     private Feld[] feld;
     private View view;
+    private String spielzustand; // "Baumodus" oder "Schiessmodus"
     
     public Spiel(){
         spieler = new Spieler[2];
@@ -19,7 +19,12 @@ public class Spiel{
     }
     
     public void setzeTreffer(int x, int y){
-        
+        if (aktuellerSpieler == 0){
+            // setzt Treffer bei Spieler1
+        }
+        else{
+            //setzt Treffer bei Spieler1
+        }
     }
     public static void beendeZug(){
     
