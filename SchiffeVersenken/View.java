475b763079a0533b396fa8cafd02bf34930a3b68
MoEck32 <129147790+MoEck32@users.noreply.github.com>
2023-05-24 08:37:03 +0200

    Update View.java

--- SchiffeVersenken/View.java
+++ SchiffeVersenken/View.java
@@ -12,115 +12,115 @@ public class View extends JFrame implements ActionListener{
     private JButton[] jButtonunten = new JButton[100];
     private JLabel[] jLabelobenEigenes = new JLabel[10];
     private JLabel[] jLabelobenGegner = new JLabel[10];
-    private JLabel[] jLabelseiteEigenes = new JLabel[10];
-    private JLabel[] jLabelseiteGegner = new JLabel[10];
-    private Spiel spiel;
+    private JLabel[] jLabelSeiteEigenes = new JLabel[10];
+    private JLabel[] jLabelSeiteGegner  = new JLabel[10];
+
+    private JLabel jLabel;
     private Feld feld;
 
- 
-    public View(Spiel spiel) {
-        this.spiel = spiel;
+    public View() {
         setSize(new Dimension(1920, 1080));
         this.setVisible(true);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
-        
+
         feld = new Feld();
-        
+
         jPaneloben = new JPanel();
         jPanelunten = new JPanel();
         jPanelrechts = new JPanel();
-        
+
         jPaneloben.setBackground(Color.green);
-        jPaneloben.setLayout(new GridLayout(12, 12));
-        jPaneloben.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 130));
+        jPaneloben.setLayout(new GridLayout(11, 11));
         jPaneloben.setSize(1600, 540);
         jPaneloben.setVisible(true);
-        
+
+        //noch zu tun!!
+        //jPaneloben.setBorder(BorderFactory.createEmptyBorder(
+
         jPanelunten.setBackground(Color.red);
-        jPanelunten.setLayout(new GridLayout(12, 12));
-        jPanelunten.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 130));
+        jPanelunten.setLayout(new GridLayout(11, 11));
         jPanelunten.setLocation(0, 540);
         jPanelunten.setSize(1600, 540);
         jPanelunten.setVisible(true);
-        
+
         jPanelrechts.setBackground(Color.yellow);
         jPanelrechts.setLayout(new GridLayout(0, 3));
         jPanelrechts.setLocation(1600, 0);
         jPanelrechts.setSize(320, 1080);
         jPanelrechts.setVisible(true);
-        
+
         for ( int i = 0; i < 10; i++){
             jLabelobenEigenes[i] = new JLabel(""+(i+1));
             jLabelobenEigenes[i].setHorizontalAlignment(SwingConstants.CENTER);
             jLabelobenGegner[i] = new JLabel(""+(i+1));
             jLabelobenGegner[i].setHorizontalAlignment(SwingConstants.CENTER);
+
         }
-        
+
         jPaneloben.add(new JLabel());
-        for (int j = 0; j < 10; j++){
-            jPaneloben.add(jLabelobenEigenes[j]);
-        }
-        for (int i = 0; i < 10; i++){
-            for (int j = 0; j < 11; j++){
-                if (j == 0){
-                    jPaneloben.add(new JLabel("A", SwingConstants.CENTER));
-                }
-                else{
-                    jButtonoben[10*i+j-1] = new JButton();
-                    jButtonoben[10*i+j-1].addActionListener(this);
-                    jPaneloben.add(jButtonoben[10*i+j-1]);
-                    
-                }
-            }
-        }
-        
         jPanelunten.add(new JLabel());
         for (int j = 0; j < 10; j++){
+            jPaneloben.add(jLabelobenEigenes[j]);
             jPanelunten.add(jLabelobenGegner[j]);
+
         }
-        for (int i = 0; i < 10; i++){
+
+        for ( int i = 0; i<10; i++ ) {
+
             for (int j = 0; j < 11; j++){
                 if (j == 0){
-                    jPanelunten.add(new JLabel("A", SwingConstants.CENTER));
-                }
-                else{
+                    char zahl=(char)(65+i);
+                    jPaneloben.add (new JLabel(Character.toString(zahl),SwingConstants.CENTER));
+                    jPanelunten.add (new JLabel(Character.toString(zahl), SwingConstants.CENTER));
+                } else{ 
+
+                    jButtonoben[10*i+j-1] = new JButton ();
+                    jPaneloben.add ( jButtonoben[10*i+j-1] );
+                    jButtonoben[10*i+j-1].addActionListener(this);
+
                     jButtonunten[10*i+j-1] = new JButton();
+                    jPanelunten.add (jButtonunten[10*i+j-1]);
                     jButtonunten[10*i+j-1].addActionListener(this);
-                    jPanelunten.add(jButtonunten[10*i+j-1]);
-                    
                 }
-            }
+            }        
+
+            
+
+            add(jPaneloben);
+            add(jPanelunten);
+            add(jPanelrechts);
         }
- 
-        add(jPaneloben);
-        add(jPanelunten);
-        add(jPanelrechts);
+
     }
- 
-    public void actionPerformed(ActionEvent e){
-        for (int i = 0; i < 100; i++){
-            if (e.getSource() == jButtonoben[i]){
+
+    public void actionPerformed(ActionEvent e)
+    {
+        for (int i= 0; i< 100; i++)
+        {
+            if ( e.getSource() == jButtonoben[i]){
                 int x = i % 10;
-                int y = i / 10;
-                System.out.println(x + "," + y);
-                if(feld.trefferEigenesFeld(x, y) == 3){
-                    jButtonoben[i].setText("X");
-                }
-                else if(feld.trefferEigenesFeld(x, y) == 1){
-                    jButtonoben[i].setText("O");
-                }
-                else {
-                    jButtonoben[i].setText("-");
-                }
+                int y = i /10;
+                System.out.println(x + "," +y);
+                // if(feld.trefferEigenesFeld(x, y) == 3) {
+                    // jButtonoben[i].setText("X");
+                // }
+
+                // else if (feld.trefferEigenesFeld(x,y) == 1){
+                    // jButtonoben[i].setText("0");
+                // }
+                // else {
+                    // jButtonoben[i].setText("-");
+                // }
+            }
+            
+            if ( e.getSource() == jButtonunten[i]){
+                int x = i % 10;
+                int y = i /10;
+                System.out.println(x + "," +y);
             }
         }
-    }
-    public void mouseClicked(MouseEvent me){
-        if (me.getJFrame == MouseEvent.BUTTON3){
-            spiel.baurichtungAendern();
-        }
+
     }
 }
-
 
